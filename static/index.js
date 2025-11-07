36b941e444c8755789e2e63b8c613ee027c5252d
milijan-mosic <milijan.mosic@gmail.com>
2025-11-07 19:13:50 +0100

    Disabled JS for now

--- static/index.js
+++ static/index.js
@@ -3,7 +3,12 @@ import LiquidBackground from "https://cdn.jsdelivr.net/npm/threejs-components@0.
 const app = LiquidBackground(document.getElementById("canvas"));
 
 app.loadImage("https://assets.codepen.io/33787/liquid.webp");
-app.liquidPlane.material.metalness = 0.75;
-app.liquidPlane.material.roughness = 0.25;
-app.liquidPlane.uniforms.displacementScale.value = 5;
-app.setRain(true);
+// app.liquidPlane.material.metalness = 0.75;
+// app.liquidPlane.material.roughness = 0.25;
+// app.liquidPlane.uniforms.displacementScale.value = 5;
+// app.setRain(true);
+
+app.liquidPlane.material.metalness = 0.0;
+app.liquidPlane.material.roughness = 0.0;
+app.liquidPlane.uniforms.displacementScale.value = 0;
+app.setRain(false);
